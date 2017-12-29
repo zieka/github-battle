e89df4a55e2c997fef18bd6f5edc6eb53b03f85d
zieka <scullykns@gmail.com>
2017-12-29 10:48:03 -0800

    refactor to es6 modules

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const api = require('../utils/api');
-const Loading = require('./Loading');
+import React from 'react';
+import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
+import Loading from './Loading';
 
 function SelectLanguage({ selectedLanguage, onSelect }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -73,9 +73,7 @@ class Popular extends React.Component {
       repos: null
     }));
 
-    api
-      .fetchPopularRepos(lang)
-      .then((repos) => this.setState(() => ({ repos })));
+    fetchPopularRepos(lang).then((repos) => this.setState(() => ({ repos })));
   }
   render() {
     const { selectedLanguage, repos } = this.state;
@@ -92,4 +90,4 @@ class Popular extends React.Component {
   }
 }
 
-module.exports = Popular;
+export default Popular;
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,10 +1,10 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const queryString = require('query-string');
-const api = require('../utils/api');
-const Link = require('react-router-dom').Link;
-const PlayerPreview = require('./PlayerPreview');
-const Loading = require('./Loading');
+import React from 'react';
+import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import { battle } from '../utils/api';
+import { Link } from 'react-router-dom';
+import PlayerPreview from './PlayerPreview';
+import Loading from './Loading';
 
 function Profile({ info }) {
   return (
@@ -61,7 +61,7 @@ class Results extends React.Component {
       this.props.location.search
     );
 
-    api.battle([playerOneName, playerTwoName]).then((players) => {
+    battle([playerOneName, playerTwoName]).then((players) => {
       if (players === null) {
         return this.setState(() => ({
           error:
@@ -103,4 +103,4 @@ class Results extends React.Component {
   }
 }
 
-module.exports = Results;
+export default Results;
